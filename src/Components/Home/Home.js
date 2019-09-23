40c4e38a0f2efbe12437eb411334fd775b1b4eb9
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-09-23 09:57:30 -0400

    modal splash and front page

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css';
+import SearchBox from './SearchBox'
+
+class Home extends React.Component{
+    // constructor(){
+        // super()
+        // this.state = {
+        // }
+    // }
+    render(){
+        return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="home col s12">
+                    <div className="upper-fold">
+                        <SearchBox />
+                    </div>
+                </div>
+            </div>
+        </div>)
+    }
+}
+export default Home;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import ModalSplash from './ModalSplash';
+import Login from './Login';
+import Signup from './Signup';
 
 class NavBar extends React.Component{
-    constructor(props){
-        super(props)
+    constructor(){
+        super()
         this.state = {
-            
+            showModal: false
         }
     }
+
+    signUp = ()=>{
+        console.log('click')
+        document.querySelector('body').className = 'body-modal-show';
+        this.setState({
+            showModal: true
+        })
+    }
+    closeModal = (e)=>{
+        document.querySelector('body').className = '';
+        this.setState({
+            showModal: false,
+            modalContent: <ModalSplash />
+        })
+    }
+
     render(){
         return(
         <div className="container-fluid nav">
             <div className="row">
-                <nav className="black">
+                <nav className="transparent">
                     <div className="nav-wrapper">
-                        <Link to="/" className="brand-logo">airbnb</Link>
+                        <Link to="/" className="left">airbnb</Link>
                         <ul id="nav-mobile" className="right">
                             <li>
                                 <Link to="/host/home">Host a Home</Link>
@@ -26,17 +45,23 @@ class NavBar extends React.Component{
                             <li>
                                 <Link to="/help">Help</Link>
                             </li>
-                            <li>
-                                <Link to="/sign-up">Sign Up</Link>
+                            <li className="nav-non-link" onClick={this.signUp} >
+                                Sign Up
                             </li>
-                            <li>
-                                <Link to="/log-in">Log In</Link>
+                            <li className="nav-non-link" onClick={this.signUp} >
+                                Log In
                             </li>
                         </ul>
                     </div>
                 </nav>
 
             </div>
+            <div className="login-modal" style={this.state.showModal ? {"display": "block"} : {}} >
+                    <button id="close-modal" onClick={this.closeModal}>&Chi;</button>
+                    <div className="modal-content">
+                        {this.state.modalContent}
+                    </div>
+                </div>
         </div>)
     }
 }
